3df1ce84c8917c0b950f751b22d1d3a9a638b255
николай тамазенко <66429266+nikos03n@users.noreply.github.com>
2023-11-10 13:34:04 +0000

    1533

--- vue-project/src/components/HelloWorld.vue
+++ vue-project/src/components/HelloWorld.vue
@@ -8,9 +8,9 @@ defineProps<{
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      123You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      Хочете зробити Ваш блокнот  ще більш особливим, підкреслити свою індивідуальність або фірмовий стиль, зробити персональний або корпоративний подарунок?
+      <a href="https://polygraphist.kiev.ua/dlya-bznesu-ta-ofsu/zapisn-knizhki/" target="_blank" rel="noopener">Виробництво</a> +
+      <a href="https://polygraphist.kiev.ua/dlya-bznesu-ta-ofsu/zapisn-knizhki/" target="_blank" rel="noopener">Виробництво</a>. What's next?
     </h3>
   </div>
 </template>
--- vue-project/src/views/AboutView.vue
+++ vue-project/src/views/AboutView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+          <a href="https://onedrive.live.com/edit.aspx?resid=534247FB5DE9C65A!6076&ithint=file%2cxlsx&wdo=2&authkey=!AqH8B5yryQnIJS0" target="_blank" rel="noopener">Vite</a> +
+
   </div>
 </template>
 
