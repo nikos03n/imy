8b4297a92cb840ff298af8bbf27b87867d832b12
николай тамазенко <66429266+nikos03n@users.noreply.github.com>
2023-11-13 13:02:06 +0000

    1311

--- vue-project/src/components/TheWelcome.vue
+++ vue-project/src/components/TheWelcome.vue
@@ -5,6 +5,8 @@ import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
+import { RouterLink, RouterView } from 'vue-router'
+
 </script>
 
 <template>
@@ -13,9 +15,11 @@ import SupportIcon from './icons/IconSupport.vue'
       <DocumentationIcon />
     </template>
     <template #heading>№1</template>
+    <RouterLink to="/about">Замовити цей виріб</RouterLink>
 
-    <a href="https://v.org/" target="_blank" rel="noopener">Іменне тиснення за 15 хвилин</a>
     За Вашим бажанням ми виконуємо тиснення імені та прізвища, ініціалів, слоганів, коротких побажань тощо. Умови:
+    
+  
   </WelcomeItem>
 
   <WelcomeItem>
@@ -61,3 +65,68 @@ import SupportIcon from './icons/IconSupport.vue'
     вартість тиснення від 100 грн
   </WelcomeItem>
 </template>
+
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
