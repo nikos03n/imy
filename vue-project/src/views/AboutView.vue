107b28b87264bc8272a9acba1b8f6d4282fa44a9
николай тамазенко <66429266+nikos03n@users.noreply.github.com>
2023-11-15 10:58:49 +0200

    232311

--- vue-project/src/views/AboutView.vue
+++ vue-project/src/views/AboutView.vue
@@ -5,11 +5,12 @@ import { RouterLink, RouterView } from 'vue-router'
   <div class="about">
     
 
-    <WelcomeItem>
+    <WelcomeItem> 
+    <RouterLink to="/">Home</RouterLink>
+
     <img alt="Vue logo" class="logo" src="https://firebasestorage.googleapis.com/v0/b/cborka.appspot.com/o/foto%2F0540_26105_1911_0.jpg?alt=media&token=beb8b885-cb6f-4fac-a1e9-9451af0bca05" width="500" height="500" />
 
     <template #heading>№1</template>
-    <RouterLink to="/">Home</RouterLink>
 
       <h4>Для оформлення замовлення необхідно заповнити коротку форму/макет <a href="https://firebasestorage.googleapis.com/v0/b/cborka.appspot.com/o/foto%2F%D0%9A%D0%BD%D0%B8%D0%B3%D0%B0firebase.xlsx?alt=media&token=63c3e144-60e8-4691-b2ae-b48646990171" target="_blank" rel="noopener">Excel</a> та відправити її за адресою <a href="" target="_blank" rel="noopener">tamazenko@polygraphist.kiev.ua</a> для узгодження</h4>
        
