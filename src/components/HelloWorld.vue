2de196fcf116af2f97323b2995cbe70dbfce7c1e
николай тамазенко <66429266+nikos03n@users.noreply.github.com>
2023-11-23 17:31:44 +0200

    1731

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,15 +4,13 @@
       class="mb-6"
       no-gutters
     >
-
-
       <v-col>
-
         <v-sheet class="pa-5 ma-5">
         <v-card
           class="mx-auto"
           max-width="344"
           hover
+          elevation="16"
         >
           <v-card-item>
             <v-card-title>
@@ -31,14 +29,13 @@
         </v-sheet>
       </v-col>
 
-
-
       <v-col>
         <v-sheet class="pa-2 ma-2">
         <v-card
           class="mx-auto"
           max-width="344"
           hover
+          elevation="16"
         >
           <v-card-item>
             <v-card-title>
@@ -92,15 +89,15 @@
             Діловий щоденник недатований. Формат 145х202 мм. Кількість сторінок 256. Блок білий офсет 70г/м2, клітинка. Колір капталу та лясе - білий. Мова укр., англ. Форзац - без друку білий. Обкладинка баладек.
           </v-card-text>
         </v-card>
-
         </v-sheet>
       </v-col>
 
       <v-col>
-        <v-sheet class="pa-10 ma-10">
+        <v-sheet class="pa-1 ma-1">
         <v-card
           class="mx-auto"
           max-width="344"
+          elevation="16"
           hover
         >
           <v-card-item>
@@ -133,7 +130,7 @@
                         <img alt="Vue logo" class="logo" src="https://polygraphist.kiev.ua/images/detailed/8482/2110_5210_s_b_1964_0.jpg" width="300" height="300" />
 
             Діловий щоденник недатований. Формат 145х202 мм. Кількість сторінок 352. Блок білий офсет 70г/м2. Колір капталу та лясе - білий. Мова укр., англ. Форзац - кольорові мапи України, Європи. Матеріал обкладинки штучна шкіра. Інформаційний блок містить:-Валюти країн світу-Позначення країн та територій в Інтернеті-Правила догляду за одягом та знаки на етикетці-Розміри одягу та взуття-Одиниці для вимірювання фізичних величин-Офіційні правила тлумачення торгівельних термінів міжнародної торгової палати Інкотермс 2020-Автомобільні коди країни-Часові пояси-Відстані між європейськими містами та між обласними центрами України-Довідкові телефони-Телефонні коди міст України-Міжнародні телефонні коди-Міжнародні штрихові коди.
-      </v-card-text>
+           </v-card-text>
 
 
         </v-card>
@@ -154,14 +151,14 @@
                 <v-card-title>
                   <span class="text-h5">Для індивідуального тиснення фольгою вам необхідно:</span>
                 </v-card-title>
-                <v-card-text>
+                <v-card-text color="red">
                 1. заповнити макет
                  <a href="https://firebasestorage.googleapis.com/v0/b/cborka.appspot.com/o/foto%2F%D0%9A%D0%BD%D0%B8%D0%B3%D0%B0firebase.xlsx?alt=media&token=63c3e144-60e8-4691-b2ae-b48646990171" target="_blank" rel="noopener">Excel</a>
                  та відправити за адресою
                   <a href="" target="_blank" rel="noopener">tamazenko@polygraphist.kiev.ua</a>
                  </v-card-text>
                  <v-card-text>
-                2. тиснення можливе безбарвне, а також золотим, червоним, срібним та білим кольором
+                2. тиснення можливе безбарвне, золотим, червоним, срібним та білим кольором
                  </v-card-text>
                                   <v-card-text>
                 3. допустима кількість символів в 1 рядку залежить від обраного формату блокнота
@@ -172,11 +169,10 @@
                                                    <v-card-text>
                 5. робота виконується лише після 100% оплати замовлення
                 </v-card-text>
-                <v-card-text>
-                                   <img alt="Vue logo" class="logo" src="https://polygraphist.kiev.ua/images/detailed/8482/2110_5210_s_b_1964_0.jpg" width="400" height="400" />
-
-                </v-card-text>
-
+                <v-sheet class="pa-2 ma-2">
+                     <img alt="Vue logo" class="logo mx-auto" src="https://polygraphist.kiev.ua/images/detailed/8482/2110_5210_s_b_1964_0.jpg" width="500" height="500" />
+                </v-sheet>
+                
                 <v-card-actions>
                   <v-spacer></v-spacer>
                   <v-btn
@@ -207,6 +203,7 @@
           class="mx-auto"
           max-width="344"
           hover
+          elevation="16"
         >
           <v-card-item>
             <v-card-title>
@@ -223,89 +220,8 @@
       </v-col>
     </v-row>
 
-    <v-row no-gutters>
-      <v-col cols="8">
-        <v-sheet class="pa-2 ma-2">
-                  <template1>
-        <v-card
-          class="mx-auto"
-          max-width="344"
-          hover
-        >
-          <v-card-item>
-            <v-card-title>
-              Card title
-            </v-card-title>
-            <v-card-subtitle>
-              Card subtitle secondary text
-            </v-card-subtitle>
-          </v-card-item>
-
-          <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </v-card-text>
-        </v-card>
-      </template1>
-
-        </v-sheet>
-      </v-col>
-
-      <v-col cols="4">
-        <v-sheet class="pa-2 ma-2">
-                  <template1>
-        <v-card
-          class="mx-auto"
-          max-width="344"
-          hover
-        >
-          <v-card-item>
-            <v-card-title>
-              Card title
-            </v-card-title>
-            <v-card-subtitle>
-              Card subtitle secondary text
-            </v-card-subtitle>
-          </v-card-item>
-
-          <v-card-text>
-                        <img alt="Vue logo" class="logo" src="https://firebasestorage.googleapis.com/v0/b/cborka.appspot.com/o/foto%2F0540_26105_1911_0.jpg?alt=media&token=beb8b885-cb6f-4fac-a1e9-9451af0bca05" width="200" height="200" />
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </v-card-text>
-        </v-card>
-      </template1>
-
-        </v-sheet>
-      </v-col>
-            <v-col cols="4">
-        <v-sheet class="pa-2 ma-2">
-                  <template1>
-        <v-card
-          class="mx-auto"
-          max-width="344"
-          hover
-        >
-          <v-card-item>https://www.ukr.net/
-            <v-card-title>
-              Card title
-            </v-card-title>
-            <v-card-subtitle>
-              Card subtitle secondary text
-            </v-card-subtitle>
-          </v-card-item>
-
-          <v-card-text>
-                        <img alt="Vue logo" class="logo" src="https://firebasestorage.googleapis.com/v0/b/cborka.appspot.com/o/foto%2F0540_26105_1911_0.jpg?alt=media&token=beb8b885-cb6f-4fac-a1e9-9451af0bca05" width="200" height="200" />
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </v-card-text>
-        </v-card>
-      </template1>
-
-        </v-sheet>
-      </v-col>
-
-    </v-row>
   </v-container2>
-  <v-container class="fill-height">
+  <!-- <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
       <v-img height="300" src="@/assets/logo.svg" />
 
@@ -372,7 +288,7 @@
         </v-col>
       </v-row>
     </v-responsive>
-  </v-container>
+  </v-container> -->
 </template>
 
 <script>
