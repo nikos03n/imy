b7a2a0cbc0ce40ecdcde54020bdd82b96707f259
николай тамазенко <66429266+nikos03n@users.noreply.github.com>
2023-12-14 10:35:12 +0200

    1034

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,7 +14,7 @@
         >
           <v-card-item>
             <v-card-title>
-              Щоденник недатований А5, 176 аркушів, крем.лінія, обкладинка штучна шкіра, бордовий, на гумці
+              Щоденник недатований А5, 128 аркушів, клітинка, обкладинка баладек зелений
             </v-card-title>
             <v-card-subtitle>
               Артикул: 241/1 06Б гумка
@@ -26,6 +26,7 @@
             Діловий щоденник недатований. Формат 145х202 мм. Кількість сторінок 352. Блок кремовий офсет 70г/м2, лінія. Колір капталу та лясе - золотий. Мова укр., англ. Перфоровані відривні кутики. Форзац - кольорові мапи Європи і України. Обкладинка гнучка, штучна шкіра, на гумці.Інформаційний блок містить:-Валюти країн світу-Позначення країн та територій в Інтернеті-Правила догляду за одягом та знаки на етикетці-Розміри одягу та взуття-Одиниці для вимірювання фізичних величин-Офіційні правила тлумачення торгівельних термінів міжнародної торгової палати Інкотермс 2020-Автомобільні коди країни-Часові пояси-Відстані між європейськими містами та між обласними центрами України-Довідкові телефони-Телефонні коди міст України-Міжнародні телефонні коди-Міжнародні штрихові коди.
           </v-card-text>
         </v-card>
+
           <v-row justify="center">
             <v-dialog
               v-model="dialog"
@@ -112,7 +113,8 @@
             <v-container align-content-center>
               <v-bottom-sheet>
                 <template v-slot:activator="{ props }">
-                  <v-btn v-bind="props" text="Замовити цей виріб"></v-btn>
+                  <v-btn
+                  color="primary" v-bind="props" text="Замовити 231 0540"></v-btn>
                 </template>
 
                 <v-card
@@ -121,10 +123,10 @@
                   imaill="tamazenko@polygraphist.kiev.ua"
                 >
                 <v-card-text>
-                1. коротка форма/макет
+                1. заповнити макет
                  <a href="https://firebasestorage.googleapis.com/v0/b/cborka.appspot.com/o/foto%2F%D0%9A%D0%BD%D0%B8%D0%B3%D0%B0firebase.xlsx?alt=media&token=63c3e144-60e8-4691-b2ae-b48646990171" target="_blank" rel="noopener">Excel</a>
-                 <!-- та відправити її за адресою
-                  <a href="" target="_blank" rel="noopener">tamazenko@polygraphist.kiev.ua</a> -->
+                 та відправити за адресою
+                  <a href="" target="_blank" rel="noopener">tamazenko@polygraphist.kiev.ua</a>
                  </v-card-text>
                  <v-card-text>
                 2. тиснення можливе безбарвне, а також золотим, червоним, срібним та білим кольором
@@ -165,27 +167,10 @@
               Щоденник недатований А5, 176 аркушів, лінія, обкладинка штучна шкіра, чорний
             </v-card-title>
             <v-card-subtitle>
-              Артикул: 241 2110            </v-card-subtitle>
+              Артикул: 241 2110
+             </v-card-subtitle>
           </v-card-item>
 
-          <!-- <v-card-text>
-                        <img alt="Vue logo" class="logo" src="https://firebasestorage.googleapis.com/v0/b/cborka.appspot.com/o/foto%2F0540_26105_1911_0.jpg?alt=media&token=beb8b885-cb6f-4fac-a1e9-9451af0bca05" width="200" height="200" />
-                                 <v-container>
-              <v-bottom-sheet>
-                <template v-slot:activator="{ props }">
-                  <v-btn v-bind="props" text="Click Me"></v-btn>
-                </template>
-
-                <v-card
-                  title="Bottom Sheet"
-                  text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut, eos? Nulla aspernatur odio rem, culpa voluptatibus eius debitis dolorem perspiciatis asperiores sed consectetur praesentium! Delectus et iure maxime eaque exercitationem!"
-                ></v-card>
-              </v-bottom-sheet>
-            </v-container>
-
-
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </v-card-text> -->
           <v-card-text>
                         <img alt="Vue logo" class="logo" src="https://polygraphist.kiev.ua/images/detailed/8482/2110_5210_s_b_1964_0.jpg" width="300" height="300" />
 
@@ -193,7 +178,7 @@
            </v-card-text>
         </v-card>
 
-          <v-row justify="center">
+          <v-row justify="center" >
             <v-dialog
               v-model="dialog"
               width="800"
@@ -203,7 +188,7 @@
                   color="primary"
                   v-bind="props"
                 >
-                  Замовити цей виріб
+                  Замовити 241 2110
                 </v-btn>
               </template>
               <v-card>
@@ -360,9 +345,180 @@
 
         </v-sheet>
       </v-col>
+
+        <v-col>
+        <v-sheet class="pa-2 ma-2">
+        <v-card
+          class="mx-auto"
+          max-width="344"
+          hover
+          elevation="16"
+        >
+          <v-card-item>
+            <v-card-title>
+              Щоденник недатований А5, 128 аркушів, клітинка, обкладинка баладек зелений
+            </v-card-title>
+            <v-card-subtitle>
+              Артикул: 241/1 06Б гумка
+            </v-card-subtitle>
+          </v-card-item>
+
+          <v-card-text>
+            <img alt="Vue logo" class="logo" src="https://firebasestorage.googleapis.com/v0/b/cborka.appspot.com/o/foto%2F06B_5209_1974_0.jpg?alt=media&token=6be7226f-0667-48a8-bb31-347eaf73c358" width="300" height="300" />
+
+            <v-container align-content-center>
+              <v-bottom-sheet>
+                <template v-slot:activator="{ props }">
+                  <v-btn
+                  color="primary" v-bind="props" text="Замовити 241/1 06Б гумка"></v-btn>
+                </template>
+
+                <v-card
+                  title="Для індивідуального тиснення фольгою на цьому виробі вам необхідно узгодити:"
+                  text=""
+                  imaill="tamazenko@polygraphist.kiev.ua"
+                >
+                <v-card-text>
+                1. заповнити макет
+                 <a href="https://firebasestorage.googleapis.com/v0/b/cborka.appspot.com/o/foto%2F%D0%9A%D0%BD%D0%B8%D0%B3%D0%B0firebase.xlsx?alt=media&token=63c3e144-60e8-4691-b2ae-b48646990171" target="_blank" rel="noopener">Excel</a>
+                 та відправити за адресою
+                  <a href="" target="_blank" rel="noopener">tamazenko@polygraphist.kiev.ua</a>
+                 </v-card-text>
+                 <v-card-text>
+                2. тиснення можливе безбарвне, а також золотим, червоним, срібним та білим кольором
+                 </v-card-text>
+                                  <v-card-text>
+                3. допустима кількість символів в 1 рядку залежить від обраного формату блокнота
+                 </v-card-text>
+                                  <v-card-text>
+                4. вартість тиснення від 100 грн + ціна блокнота
+                 </v-card-text>
+                                                   <v-card-text>
+                5. робота виконується лише після 100% оплати замовлення
+                 </v-card-text>
+
+
+                  <img alt="Vue logo" class="logo" src="https://firebasestorage.googleapis.com/v0/b/cborka.appspot.com/o/foto%2F06B_5209_1974_0.jpg?alt=media&token=6be7226f-0667-48a8-bb31-347eaf73c358" width="300" height="300" />
+
+                </v-card>
+              </v-bottom-sheet>
+            </v-container>
+
+                        Діловий щоденник недатований. Формат 145х202 мм. Кількість сторінок 352. Блок кремовий офсет 70г/м2, лінія. Колір капталу та лясе - золотий. Мова укр., англ. Перфоровані відривні кутики. Форзац - кольорові мапи Європи і України. Обкладинка гнучка, штучна шкіра, на гумці.Інформаційний блок містить:-Валюти країн світу-Позначення країн та територій в Інтернеті-Правила догляду за одягом та знаки на етикетці-Розміри одягу та взуття-Одиниці для вимірювання фізичних величин-Офіційні правила тлумачення торгівельних термінів міжнародної торгової палати Інкотермс 2020-Автомобільні коди країни-Часові пояси-Відстані між європейськими містами та між обласними центрами України-Довідкові телефони-Телефонні коди міст України-Міжнародні телефонні коди-Міжнародні штрихові коди.
+
+          </v-card-text>
+        </v-card>
+        </v-sheet>
+      </v-col>
+
+
     </v-row>
 
   </v-container2>
+
+  <v-container2 class="bg-surface-variant">
+    <v-row
+      class="mb-6"
+      no-gutters
+    >
+
+      <v-col>
+        <v-sheet class="pa-1 ma-1">
+        <v-card
+          class="mx-auto"
+          max-width="344"
+          elevation="16"
+          hover
+        >
+          <v-card-item>
+            <v-card-title>
+              Щоденник недатований А5, 176 аркушів, крем.лінія, обкладинка штучна шкіра, бордовий, на гумці
+            </v-card-title>
+            <v-card-subtitle>
+              Артикул: 241/1 06Б           </v-card-subtitle>
+          </v-card-item>
+
+          <v-card-text>
+                        <img alt="Vue logo" class="logo" src="https://firebasestorage.googleapis.com/v0/b/cborka.appspot.com/o/foto%2F06B_5209_1974_0.jpg?alt=media&token=6be7226f-0667-48a8-bb31-347eaf73c358" width="300" height="300" />
+
+            Діловий щоденник недатований. Формат 145х202 мм. Кількість сторінок 352. Блок кремовий офсет 70г/м2, лінія. Колір капталу та лясе - золотий. Мова укр., англ. Перфоровані відривні кутики. Форзац - кольорові мапи Європи і України. Обкладинка гнучка, штучна шкіра, на гумці.Інформаційний блок містить:-Валюти країн світу-Позначення країн та територій в Інтернеті-Правила догляду за одягом та знаки на етикетці-Розміри одягу та взуття-Одиниці для вимірювання фізичних величин-Офіційні правила тлумачення торгівельних термінів міжнародної торгової палати Інкотермс 2020-Автомобільні коди країни-Часові пояси-Відстані між європейськими містами та між обласними центрами України-Довідкові телефони-Телефонні коди міст України-Міжнародні телефонні коди-Міжнародні штрихові коди.
+           </v-card-text>
+        </v-card>
+
+          <v-row justify="center">
+            <v-dialog
+              v-model="dialog"
+              width="800"
+            >
+              <template v-slot:activator="{ props }">
+                <v-btn
+                  color="primary"
+                  v-bind="props"
+                >
+                  Замовити 241/1 06Б
+                </v-btn>
+              </template>
+              <v-card>
+                <v-card-title>
+                  <span class="text-h5">Для індивідуального тиснення фольгою вам необхідно:</span>
+                </v-card-title>
+                <v-card-text color="red">
+                1. заповнити макет
+                 <a href="https://firebasestorage.googleapis.com/v0/b/cborka.appspot.com/o/foto%2F%D0%9A%D0%BD%D0%B8%D0%B3%D0%B0firebase.xlsx?alt=media&token=63c3e144-60e8-4691-b2ae-b48646990171" target="_blank" rel="noopener">Excel</a>
+                 та відправити за адресою
+                  <a href="" target="_blank" rel="noopener">tamazenko@polygraphist.kiev.ua</a>
+                 </v-card-text>
+                 <v-card-text>
+                2. тиснення можливе безбарвне, золотим, червоним, срібним та білим кольором
+                 </v-card-text>
+                                  <v-card-text>
+                3. допустима кількість символів в 1 рядку залежить від обраного формату блокнота
+                 </v-card-text>
+                                  <v-card-text>
+                4. вартість тиснення від 100 грн + ціна блокнота
+                 </v-card-text>
+                                                   <v-card-text>
+                5. робота виконується лише після 100% оплати замовлення
+                </v-card-text>
+                <v-sheet class="pa-2 ma-2">
+                     <img alt="Vue logo" class="logo mx-auto" src="https://firebasestorage.googleapis.com/v0/b/cborka.appspot.com/o/foto%2F06B_5209_1974_0.jpg?alt=media&token=6be7226f-0667-48a8-bb31-347eaf73c358" width="500" height="500" />
+                </v-sheet>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    color="green-darken-1"
+                    variant="text"
+                    @click="dialog = false"
+                    href=""
+                  >
+                    Disagree
+                  </v-btn>
+                  <v-btn
+                    color="green-darken-1"
+                    variant="text"
+                    @click="dialog = false"
+                  >
+                    Agree
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-row>
+
+        </v-sheet>
+      </v-col>
+
+    </v-row>
+
+  </v-container2>
+
+
+
+
+
+
+
   <!-- <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
       <v-img height="300" src="@/assets/logo.svg" />
